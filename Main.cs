a8069e6f4388cacdf70404a7a63046c4b1b40c58
BIDA Mohamed Amine <moumouh206@gmail.com>
2020-12-12 22:04:06 +0100

    1st test worked

--- Main.cs
+++ Main.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using TMDbLib.Client;
 using TMDbLib.Objects.General;
@@ -12,6 +13,9 @@ namespace Movies_poster_downloader
 {
     public partial class Main : Form
     {
+        [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern void SHChangeNotify(
+            int wEventId, int uFlags, IntPtr dwItem1, IntPtr dwItem2);
         bool busy = false;
         int total = 0;
         public Main()
@@ -77,60 +81,79 @@ namespace Movies_poster_downloader
 
         private void StartDownloadBtn_Click(object sender, EventArgs e)
         {
-
             if (total > 0)
             {
                 progressBar1.Value = 0;
                 try
                 {
-                    // MessageBox.Show(listView1.Items.Count.ToString());
                     for (int i = 0; i < listView1.Items.Count; i++)
                     {
+                        string dir = listView1.Items[i].SubItems[2].Text;
                         progressBar1.Value += 1;
-                        try
+                        if (!File.Exists(dir + @"\poster.ico"))
                         {
-                            bool found = false;
-                            TMDbClient client = new TMDbClient("3107e4ce8b2ea681bbd58cb0208968ea");
-                            WebClient webClient = new WebClient();
-                            SearchContainer<SearchMovie> results = client.SearchMovieAsync(listView1.Items[i].SubItems[0].Text).Result;
-                            listView1.Items[i].SubItems[1].Text = "Searching ...";
-                            listView1.Refresh();
-                            string uri = string.Empty;
-                            foreach (SearchMovie result in results.Results)
-                            {
-                                //MessageBox.Show(result.PosterPath);
-                                if (result.PosterPath != string.Empty)
-                                {
-                                    listView1.Items[i].SubItems[1].Text = "Downloading ...";
-                                    uri = "https://image.tmdb.org/t/p/w500" + result.PosterPath;
-                                    Clipboard.SetText(uri);
-                                    byte[] bytes = webClient.DownloadData(uri);
-                                    MemoryStream ms = new MemoryStream(bytes);
-                                    Image artWork = Image.FromStream(ms);
-                                    artWork.Save(listView1.Items[i].SubItems[2].Text+"\\"+result.Id.ToString() + ".jpg");
-                                    listView1.Items[i].SubItems[1].Text = "Done";
-                                    found = true;
-                                    listView1.Refresh();
-                                    break;
-                                }
-                                listView1.Items[i].SubItems[1].Text = "Not found.";
-                                listView1.Refresh();
-
-                            }
-                            if (!found)
-                            {
-                                listView1.Items[i].SubItems[1].Text = "Not found.";
-                                listView1.Refresh();
-                            }
                             
-
+                            try
+                            {
+                                bool found = false;
+                                TMDbClient client = new TMDbClient("3107e4ce8b2ea681bbd58cb0208968ea");
+                                WebClient webClient = new WebClient();
+                                SearchContainer<SearchMovie> results = client.SearchMovieAsync(listView1.Items[i].SubItems[0].Text).Result;
+                                listView1.Items[i].SubItems[1].Text = "Searching ...";
+                                listView1.Refresh();
+                                string uri = string.Empty;
+                                foreach (SearchMovie result in results.Results)
+                                {
+                                    if (result.PosterPath != string.Empty)
+                                    {
+                                        listView1.Items[i].SubItems[1].Text = "Downloading ...";
+                                        uri = "https://image.tmdb.org/t/p/w500" + result.PosterPath;
+                                        //Clipboard.SetText(uri);
+                                        try
+                                        {
+                                            byte[] bytes = webClient.DownloadData(uri);
+                                            MemoryStream ms = new MemoryStream(bytes);
+                                            Image artWork = Image.FromStream(ms);
+                                            artWork.Save(dir + "\\" + result.Id.ToString() + ".jpg");
+                                            //artWork.Save(dir + "\\poster.ico", System.Drawing.Imaging.ImageFormat.Icon);
+                                            ImagingHelper.ConvertToIcon(dir + "\\" + result.Id.ToString() + ".jpg", dir + "\\poster.ico", 256, false);
+                                            string[] lines = { "[.ShellClassInfo]", "IconResource=poster.ico,0" };
+                                            File.WriteAllLines(dir + @"\desktop.ini", lines);
+                                            File.SetAttributes(dir + @"\desktop.ini", File.GetAttributes(dir + @"\desktop.ini") | FileAttributes.Hidden | FileAttributes.ReadOnly);
+                                            File.SetAttributes(dir + @"\poster.ico", File.GetAttributes(dir + @"\poster.ico") | FileAttributes.Hidden | FileAttributes.ReadOnly);
+                                            File.SetAttributes(dir, File.GetAttributes(dir) | FileAttributes.ReadOnly);
+                                            SHChangeNotify(0x08000000, 0x0000, (IntPtr)null, (IntPtr)null);
+                                            listView1.Items[i].SubItems[1].Text = "Done";
+                                            found = true;
+                                            listView1.Refresh();
+                                            break;
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            listView1.Items[i].SubItems[1].Text = ex.Message;
+                                            // MessageBox.Show(ex.Message);
+                                            // break;
+                                        }
+                                    }
+                                }
+                                if (!found && !File.Exists(listView1.Items[i].SubItems[2].Text + @"\poster.ico"))
+                                {
+                                    listView1.Items[i].SubItems[1].Text = "Not found.";
+                                    listView1.Refresh();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
                         }
-                        catch (Exception ex)
+                        else
                         {
-
-                            MessageBox.Show(ex.Message);
+                            listView1.Items[i].SubItems[1].Text = "Icon already exist";
+                            listView1.Items[i].SubItems[1].ForeColor = Color.LightGray;
                         }
                     }
+
                 }
                 catch (Exception ex)
                 {
